3cae960197daa237138c7f82b539ab263127d9a8
Praisg <gavipraise@gmail.com>
2023-04-27 16:20:13 -0700

    again

--- main.c
+++ main.c
@@ -23,14 +23,15 @@ char *index;
   char **argz = NULL;
 int num_index = 0;
 int y;
-bool pipee = false;
+/*bool pipee = false;*/
 (void) ac;
 (void) env;
-while (1 && !pipee)
+while (1)/* && !pipee)
 {
 	if (isatty(STDIN_FILENO) == 0)
-		pipee = true;
-write(STDOUT_FILENO, instant, 8);
+		pipee = true;*/
+{
+printf("%s", instant);
 sizchar = getline(&bufAdd, &siz, stdin);
 
 if (sizchar == -1)
